b6b2451e3263019a4d4487152ce8dd3786570b4f
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2025-01-25 14:55:25 +0000

    Deploying to gh-pages from @ Muhaddil/saed-web-page@436d2aec24516597f32270164f15c3ded6eea8d7 🚀

--- assets/QCardActions-B5O7GtD-.js
+++
@@ -1,1 +0,0 @@
-import{f as e,i as r,h as c}from"./QBtn-BrR4Mgzp.js";import{i as n,h as i,m as l}from"./main-CAeg0-fQ.js";const d=n({name:"QCardActions",props:{...e,vertical:Boolean},setup(a,{slots:s}){const o=r(a),t=i((()=>`q-card__actions ${o.value} q-card__actions--${!0===a.vertical?"vert column":"horiz row"}`));return()=>l("div",{class:t.value},c(s.default))}});export{d as Q};
