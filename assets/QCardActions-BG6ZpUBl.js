0c3a004d508e2d12b69b7ad562d805b7274f79fa
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2025-05-17 16:11:04 +0000

    Deploying to gh-pages from @ Muhaddil/saed-web-page@43080833d51eee6877a90e2aec01b7d791d3fd54 🚀

--- assets/QCardActions-BG6ZpUBl.js
+++
@@ -1,1 +0,0 @@
-import{e,f as r,h as c}from"./QBtn-BLG39Sjg.js";import{g as n,k as i,i as l}from"./main-CeMCSvDI.js";const d=n({name:"QCardActions",props:{...e,vertical:Boolean},setup(a,{slots:s}){const o=r(a),t=i((()=>`q-card__actions ${o.value} q-card__actions--${!0===a.vertical?"vert column":"horiz row"}`));return()=>l("div",{class:t.value},c(s.default))}});export{d as Q};
---
+++ assets/QCardActions-D30vxH_R.js
@@ -0,0 +1,1 @@
+import{e,f as r,h as c}from"./QBtn-DD3SWXr9.js";import{g as n,k as i,i as l}from"./main-BSbXOBpI.js";const d=n({name:"QCardActions",props:{...e,vertical:Boolean},setup(a,{slots:s}){const o=r(a),t=i((()=>`q-card__actions ${o.value} q-card__actions--${!0===a.vertical?"vert column":"horiz row"}`));return()=>l("div",{class:t.value},c(s.default))}});export{d as Q};
